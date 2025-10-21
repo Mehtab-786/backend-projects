9aca8d32dfcdb47bcde8292eed53dd0c1396a31a
Mehtab Hussain <tradercoder786@gmail.com>
2025-10-21 22:26:08 +0530

    generate refresh and access token, verifiaction

--- heavyProject/src/routes/user.routes.js
+++ heavyProject/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { loginUser, logoutUser, registerUser } from '../controllers/user.controllers.js';
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.controllers.js';
 import { upload } from '../middlewares/multer.middlewares.js';
 import { verifyJWT } from '../middlewares/auth.middlewares.js';
 
@@ -22,10 +22,11 @@ router.route('/register').post(
 
 router.route('/login').post(
     loginUser
-)
+);
 
 //secured routes
-router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/refresh-token').post(refreshAccessToken);
 
 
 
