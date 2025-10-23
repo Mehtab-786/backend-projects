d1fc30f23b974c9115c29ee136c1569e2bda4600
Mehtab Hussain <tradercoder786@gmail.com>
2025-10-23 19:25:06 +0530

    Add subscription model

---
+++ heavyProject/src/models/subscriptions.models.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User' //one who is subscribing
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User' // one whose channel got subscribed 
+    }
+}, {timestamps:true});
+
+export const Subscription = mongoose.model('Subscription', subscriptionSchema)
