becf4e89ed39d3138eac8439f048875b19308074
Mehtab Hussain <tradercoder786@gmail.com>
2025-11-20 18:39:16 +0530

    Routes completed

---
+++ taskflow/src/controllers/task.controllers.js
@@ -0,0 +1,78 @@
+import { taskModel } from '../models/task.models.js';
+import mongoose from 'mongoose';
+
+async function taskCreate(req, res) {
+    try {
+        const { task } = req.body;
+
+        let taskAlreadyCreated = await taskModel.findOne({task})
+
+        if(taskAlreadyCreated){
+            return res.status(400).json({
+                message:"Task already created"
+            })
+        }
+
+        if (!task || task.length <= 2) {
+            return res.status(400).json({
+                message: 'Task is required or task is too short'
+            });
+        };
+        const resp = await taskModel.create({
+            task,
+            completed: false
+        });
+
+        return res.status(201).json({
+            message: 'Task created successfully',
+            resp
+        });
+    } catch (error) {
+        console.log('Error while creating task :: ', error)
+    }
+};
+
+
+async function allTask(req, res) {
+    const tasks = await taskModel.find().select("-createdAt -updatedAt -__v ");
+
+    return res.status(200).json({
+        message: "All task",
+        tasks
+    });
+};
+
+async function deleteTask(req, res) {
+    try {
+        const params = req?.params?.id
+
+        if (!mongoose.isValidObjectId(params)) {
+            return res.status(400).json({
+                message: 'Not valid id'
+            })
+        };
+        await taskModel.findByIdAndDelete(params);
+
+        return res.status(200).json({
+            message: "Task deleted succesfully"
+        });
+    } catch (error) {
+        console.log('Valid Id not provided')
+        return;
+    }
+};
+
+
+async function updateTask(req, res) {
+    const params = req?.params?.id;
+    const { task } = req.body;
+
+    const taskEdited = await taskModel.findByIdAndUpdate(params, { task }, { new: true });
+
+    res.status(200).json({
+        message: 'Task updated successfully',
+        taskEdited
+    });
+}
+
+export {allTask, deleteTask, taskCreate, updateTask}
--- taskflow/src/routes/task.routes.js
+++ taskflow/src/routes/task.routes.js
@@ -1,75 +1,14 @@
 import { Router } from 'express';
-import { taskModel } from '../models/task.models.js';
-import mongoose from 'mongoose';
+import { allTask, deleteTask, taskCreate, updateTask } from '../controllers/task.controllers.js';
 
 const routes = Router();
 
-routes.post('/', async (req, res) => {
-    try {
-        const { task } = req.body;
+routes.post('/', taskCreate);
 
-        if (!task || task.length <= 2) {
-            return res.status(400).json({
-                message: 'Task is required or task is too short'
-            });
-        };
+routes.get('/tasks', allTask)
 
-        const resp = await taskModel.create({
-            task,
-            completed: false
-        })
+routes.delete('/delete/:id', deleteTask);
 
-        return res.status(201).json({
-            message: 'Task created successfully',
-            resp
-        });
-    } catch (error) {
-        console.log('Error while creating task :: ', error)
-    }
-});
-
-routes.get('/tasks', async (req, res) => {
-    const tasks = await taskModel.find().select("-createdAt -updatedAt -__v -_id");
-
-    return res.status(200).json({
-        message: "All task",
-        tasks
-    })
-
-})
-
-routes.delete('/delete/:id', async (req, res) => {
-    try {
-        const params = req?.params?.id
-
-        if (!mongoose.isValidObjectId(params)) {
-            return res.status(400).json({
-                message: 'Not valid id'
-            })
-        }
-
-        await taskModel.findByIdAndDelete(params)
-
-        return res.status(200).json({
-            message: "Task deleted succesfully"
-        });
-    } catch (error) {
-        console.log('Valid Id not provided')
-        return;
-    }
-});
-
-routes.patch('/update/:id', async (req, res) => {
-    const params = req?.params?.id;
-    const { task } = req.body;
-
-    const taskEdited = await taskModel.findByIdAndUpdate(params, { task }, { new: true });
-
-    res.status(200).json({
-        message: 'Task updated successfully',
-        taskEdited
-    });
-
-})
+routes.patch('/update/:id', updateTask)
 
 export default routes;
