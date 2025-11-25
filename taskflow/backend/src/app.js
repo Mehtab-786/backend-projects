7a91d0383b2b04ebaea25bfb7e40160e0901ea90
Mehtab Hussain <tradercoder786@gmail.com>
2025-11-25 19:56:08 +0530

    Auth middleware backend

--- taskflow/backend/src/app.js
+++ taskflow/backend/src/app.js
@@ -2,15 +2,17 @@ import { config } from 'dotenv';
 config();
 import cors from 'cors'
 import express from 'express';
-import taskRoutes from './routes/task.routes.js'
-
+import taskRoutes from './routes/task.routes.js';
+import userRoutes from './routes/user.routes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors({ origin: '*' }))
-
+app.use(cookieParser())
 app.use(express.json());
 
-app.use('/api/task', taskRoutes)
+app.use('/api/task', taskRoutes);
+app.use('/api/user', userRoutes);
 
 export default app;
